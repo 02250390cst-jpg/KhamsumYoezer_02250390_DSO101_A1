cdf9aef9e98789e82e6194dd7ea09c8ec455dfcb
02250390cst-jpg <02250390.cst@rub.edu.bt>
2026-05-08 14:51:27 +0600

    c

--- DSO101/DSO101_02250390_A1/todo-app/backend/server.js
+++ DSO101/DSO101_02250390_A1/todo-app/backend/server.js
@@ -13,6 +13,10 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD || "postgres",
   database: process.env.DB_NAME || "todo",
   port: Number(process.env.DB_PORT) || 5432,
+  ssl:
+    process.env.DB_SSL === "true"
+      ? { rejectUnauthorized: false }
+      : undefined,
 });
 
 // Create Task
